eb37015f2321d73b1ecf405fb89aa777e8b3d440
QDL <919527688@qq.com>
2019-12-26 18:42:31 +0800

    change interface

--- HAL/src/linux/platform/standard.h
+++ HAL/src/linux/platform/standard.h
@@ -2,8 +2,8 @@
 
 // configure this to match the output of `ip a`
 const char *interfaces[N_IFACE_ON_BOARD] = {
-    "veth-r2-r1",
-    "veth-r2-r3",
+    "eth1",
+    "eth2",
     "eth3",
     "eth4",
 };
